423ea12f02660ee57367bdb3b202cdff0e5b6c07
JoaoHSantana2007 <jhfcsant2007@gmail.com>
2025-08-20 20:28:37 -0300

    add Aula 01

---
+++ Aulas/Aula_01/Ola_mundo/Ola_mundo.java
@@ -0,0 +1,13 @@
+package Ola_mundo;
+
+import javax.swing.JOptionPane;
+
+public class Ola_mundo {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Ola, mundo");
+		
+	}
+
+}
---
+++ Aulas/Aula_01/calculadora/Calculadora.java
@@ -0,0 +1,11 @@
+package calculadora;
+
+public class Calculadora {
+	
+	public Double numeroA;
+	public Double numeroB;
+	
+	public void somar() {
+		System.out.printf("%.2f + %.2f = %.2f %n", numeroA, numeroB, numeroA + numeroB);
+	}
+}
